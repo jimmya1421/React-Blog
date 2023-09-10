7d986941a098a0cd83784604fb67610844556c5a
Omee_21 <98630920+Om2123@users.noreply.github.com>
2023-09-10 19:50:58 +0530

    Update About.jsx

--- src/compo/About.jsx
+++ src/compo/About.jsx
@@ -4,10 +4,11 @@ const About = () => {
     return (
         <div className="container mx-auto p-8">
        
-          <form netlify>
+          <form netlify  data-netlify="true" method="post">
   <input type="text" name="name" />
   <input type="email" name="email" />
   <input type="submit" value="Submit" />
+              <button>click here</button>
 </form>
 
       </div>
